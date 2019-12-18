86d20bc0d99d67a40d34aceadebbe73fa8007b97
Jack S. Hale <mail@jackhale.co.uk>
2019-12-18 16:47:41 +0100

    Fix exact solution

--- demo/goal_oriented_adaptivity/exact_solution.h
+++ demo/goal_oriented_adaptivity/exact_solution.h
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <boost/math/constants/constants.hpp>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
@@ -7,6 +8,8 @@
 #include <dolfin/mesh/MeshFunction.h>
 
 namespace py = pybind11;
+
+const double pi_2 = boost::math::constants::pi<double>()/2.0;
 
 class Exact : public dolfin::Expression
 {
@@ -18,7 +21,7 @@ public:
   void eval(Eigen::Ref<Eigen::VectorXd> values, Eigen::Ref<const Eigen::VectorXd> x, const ufc::cell& cell) const override
   {
     double r = sqrt(x[0]*x[0] + x[1]*x[1]);
-    double theta = atan2(x[1], x[0]);
+    double theta = atan2(x[1], x[0]) + pi_2;
     values[0] = pow(r, 2.0/3.0)*sin((2.0/3.0)*theta);
   }
 };
