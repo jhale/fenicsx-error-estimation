e117616643c111845060a3cbac2660da0e707aee
Jack S. Hale <mail@jackhale.co.uk>
2019-12-20 17:55:58 +0100

    Maximum marking strategy works with MPI.

--- bank_weiser/__init__.py
+++ bank_weiser/__init__.py
@@ -1,3 +1,3 @@
 from bank_weiser.interpolate import create_interpolation
 from bank_weiser.estimate import estimate, weighted_estimate
-from bank_weiser.mark import mark
+from bank_weiser.mark import mark, maximum
--- bank_weiser/mark.py
+++ bank_weiser/mark.py
@@ -1,6 +1,11 @@
+import numpy as np
+import mpi4py.MPI as MPI
 from dolfin import MeshFunction
 
 def mark(eta_h, theta):
+    if (eta_h.function_space().mesh().mpi_comm().size > 1):
+        raise SystemExit("Does not work with with MPI size > 1")
+
     etas = eta_h.vector().get_local()
     indices = etas.argsort()[::-1]
     sorted = etas[indices]
@@ -20,3 +25,16 @@ def mark(eta_h, theta):
 
     return markers
 
+def maximum(eta_h, theta):
+    mesh = eta_h.function_space().mesh()
+    etas = eta_h.vector().get_local()
+
+    eta_max = eta_h.vector().max()
+    frac = theta*eta_max
+
+    markers = MeshFunction("bool", mesh, mesh.geometry().dim(), False)
+    marked = np.zeros_like(etas, dtype=np.bool)
+    marked[np.where(etas > frac)] = True
+    markers.set_values(marked)
+
+    return markers
