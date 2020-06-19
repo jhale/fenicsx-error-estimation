ee163c6cbb2ccbcab6d89d75d737d25132adc9d4
Jack S. Hale <mail@jackhale.co.uk>
2020-06-19 15:24:47 +0200

    Use dolfinx image

--- launch-container.sh
+++ launch-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm -v "$(pwd)":/home/fenics/shared -w /home/fenics/shared jhale/fenics-error-estimation:latest
+docker run -ti --rm -v "$(pwd)":/root/shared -w /root/shared dolfinx/dolfinx
