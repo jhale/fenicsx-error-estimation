f9eabfe436fc6ff5d7cf30af0d50f2b169b29a09
Jack S. Hale <mail@jackhale.co.uk>
2021-11-04 12:42:16 +0100

    Try tests

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -5,7 +5,7 @@ CONTAINER_ENGINE="podman"
 
 ${CONTAINER_ENGINE} pull fenicsproject/test-env:latest-mpich
 ${CONTAINER_ENGINE} pull dolfinx/dev-env
-${CONTAINER_ENGINE} build --no-cache -t jhale/dolfinx:debug .
+${CONTAINER_ENGINE} build --no-cache -t jhale/fenicsx-error-estimation:debug .
 ${CONTAINER_ENGINE} build --no-cache \
    --build-arg CMAKE_BUILD_TYPE=RelWithDebug --build-arg PIP_EXTRA_FLAGS='' \
    --build-arg CXXFLAGS='-O2 -g' --build-arg IMAGE=dolfinx/dev-env -t jhale/fenicsx-error-estimation:release .
