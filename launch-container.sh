7a138d764fabe0827dad68f2cdd9b3e1fd2f4452
Jack S. Hale <mail@jackhale.co.uk>
2022-07-11 16:57:19 +0200

    Tweak

--- launch-container.sh
+++ launch-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm -v "$(pwd)":/root/shared -w /root/shared jhale/fenicsx-error-estimation:release
+podman run -ti --rm -v "$(pwd)":/root/shared -w /root/shared jhale/fenicsx-error-estimation:debug
