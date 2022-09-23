6ffca8022dc14c9f05396f43a2ed85bf16368b3a
Jack S. Hale <mail@jackhale.co.uk>
2022-09-23 19:54:25 +0200

    Updated Dockerfile

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -3,7 +3,7 @@ set -e
 #CONTAINER_ENGINE="podman"
 CONTAINER_ENGINE="docker"
 
-${CONTAINER_ENGINE} build --no-cache -t jhale/fenicsx-error-estimation:debug .
-${CONTAINER_ENGINE} build --no-cache \
+${CONTAINER_ENGINE} build -t jhale/fenicsx-error-estimation:debug .
+${CONTAINER_ENGINE} build \
    --build-arg CMAKE_BUILD_TYPE=RelWithDebug --build-arg PIP_EXTRA_FLAGS='' \
    --build-arg CXXFLAGS='-O3 -g' --build-arg IMAGE=dolfinx/dev-env:v0.5.1-r1 -t jhale/fenicsx-error-estimation:release .
