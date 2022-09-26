4e3a20f91f61a5ca134887dbd0fe30d2284e6300
Jack S. Hale <mail@jackhale.co.uk>
2022-09-26 09:18:16 +0200

    Add slope

--- demo/fractional_adaptive_checkerboard/plot_convergence.py
+++ demo/fractional_adaptive_checkerboard/plot_convergence.py
@@ -31,8 +31,10 @@ plt.loglog(df["dof num"], df["L2 bw"], '^-',
            label=r"$\eta^{\mathrm{bw}}_{\kappa}$", color=dark_map[3])
 plt.xlabel("dofs")
 plt.ylabel(r"$\eta^{\mathrm{bw}}_{\kappa}$")
+marker_x, marker_y = marker([0.5, 0.2], [df["dof num"].median(), df["dof num"].tail(1).item()], [
                             df["L2 bw"].median(), df["L2 bw"].tail(1).item()])
 annotation.slope_marker((marker_x, marker_y), (-1, 1), invert=True)
+print(np.polyfit(np.log10(df["dof num"])[:-5:-1], np.log10(df["L2 bw"])[:-5:-1], 1)) 
 #plt.legend()
 plt.tight_layout()
 plt.savefig("output/error.pdf")
