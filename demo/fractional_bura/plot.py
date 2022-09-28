10fc62fff79902dff037018683e8fad0df89ca7a
Raphaël Bulle <rbulle@tutanota.com>
2022-09-28 10:14:34 -0400

    Add plot script to demo-bura

---
+++ demo/fractional_bura/plot.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpltools import annotation
+
+def marker(x_data, y_datas, position, gap):
+    middle = np.floor(len(x_data)/2.).astype(int)
+    anchor_1_1 = []
+    anchor_2_1 = []
+
+    for data in y_datas:
+        anchor_1_1.append(data[middle])
+        anchor_2_1.append(data[-1])
+    anchor_1_1 = min(anchor_1_1)
+    anchor_2_1 = min(anchor_2_1)
+
+    anchor_1_0 = x_data[middle]
+    anchor_2_0 = x_data[-1]
+
+    anchor_1 = [anchor_1_0, anchor_1_1]
+    anchor_2 = [anchor_2_0, anchor_2_1]
+    marker_x = anchor_1[0]**position*anchor_2[0]**(1.-position)\
+        * (anchor_2[1]/anchor_1[1])**gap
+    marker_y = anchor_1[1]**position*anchor_2[1]**(1.-position)\
+        * (anchor_1[0]/anchor_2[0])**gap
+    return marker_x, marker_y
+
+def plot(xs, ys, method):
+    A = np.vstack([np.log(xs), np.ones_like(xs)]).T
+
+    m = np.linalg.lstsq(A, np.log(ys), rcond=None)[0][0]
+    print(f"Estimator slope ({method}): {m}")
+
+    last_ys = ys[::-5]
+    print(last_ys)
+    plt.loglog(xs, ys, "^--", label=f"L2 bw ({method})")
+
+
+df_bp = pd.read_csv(f"./results_bp.csv")
+df_bura = pd.read_csv(f"./results_bura.csv")
+
+xs_bp = df_bp["dof num"].values
+ys_bp = df_bp["L2 bw"].values
+
+xs_bura = df_bura["dof num"].values
+ys_bura = df_bura["L2 bw"].values
+
+plt.figure()
+plot(xs_bp, ys_bp, "bp")
+plot(xs_bura, ys_bura, "bura")
+plt.loglog(xs_bp, xs_bp**(-1.), "-", label=f"slope -1")
+plt.legend()
+plt.xlabel("dof")
+plt.ylabel("L2 bw")
+plt.savefig(f"conv.pdf")
